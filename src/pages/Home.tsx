93b10c78a1113411af69bfa34e752b6e618e02ca
ArnoVoxel <jimmymoliere@gmail.com>
2024-12-10 00:42:48 +0100

    Add loading state to Home component while fetching group members

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [groupMembers, setGroupMembers] = useState<string[]>([]);
   // using env variable
   console.log(import.meta.env.VITE_SANTA_BACK_URL);
@@ -14,13 +15,15 @@ const Home: React.FC = () => {
       const token = localStorage.getItem("token");
       console.log("token : ", token);
       // if (token) {
-        try {
+      try {
+        setIsLoading(true);
           // const response = await axios.get(apiUrl + "api/users/group/1", {
           const response = await axios.get(apiUrl + "api/users/", {
             // headers: { Authorization: `Bearer ${token}` },
           });
           console.log("after request : ", response);
-          setGroupMembers(response.data);
+        setGroupMembers(response.data);
+        setIsLoading(false);
         } catch (err) {
           console.error("Erreur lors de la récupération des membres du groupe.", err);
         }
@@ -32,13 +35,18 @@ const Home: React.FC = () => {
 
   return (
     <div>
-      <h2>Bienvenue dans votre groupe Secret Santa !</h2>
-      <h3>Lutins malins du groupe 1 </h3>
-      <ul>
-        {groupMembers.map((email) => (
-          <li key={email}>{email}</li>
-        ))}
-      </ul>
+      {isLoading && <p>Chargement en cours...</p>}
+      {!isLoading &&
+        <>
+        <h2>Bienvenue dans votre groupe Secret Santa !</h2>
+        <h3>Lutins malins du groupe 1 </h3>
+        <ul>
+          {groupMembers.map((email) => (
+            <li key={email}>{email}</li>
+          ))}
+        </ul>
+        </>
+      }
     </div>
   );
 };
