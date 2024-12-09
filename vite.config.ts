ebead5c3d08c88bbfdafb5329634ea80d908f993
ArnoVoxel <jimmymoliere@gmail.com>
2024-12-09 21:52:35 +0100

    Update base path for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/secrat-santa-front/',
+  base: '/secrat-santa-front.github.io/',
   plugins: [react()],
 })
